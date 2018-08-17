b9612c5f11bf7ba1dd1dac8749689ef472e2068f
Alex Tsiras <tsirasalexis@yahoo.gr>
2018-08-18 00:13:10 +0300

    Update config with env variables

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,8 +2,8 @@ ShopifyApp.configure do |config|
   include ActionView::Helpers::AssetUrlHelper
 
   config.application_name = "My Shopify App"
-  config.api_key = "216cadca2dcdbc17a64f464c03f76e1a"
-  config.secret = "f78b4396435db2b7905c5fbe366340c9"
+  config.api_key = ENV['API_KEY']
+  config.secret = ENV['API_SECRET']
   config.scope = "read_orders, read_products, write_script_tags"
   config.embedded_app = true
   config.after_authenticate_job = false
