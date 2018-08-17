5a4687f1e6fb298396bec150a9fcd454248286f3
Alex Tsiras <tsirasalexis@yahoo.gr>
2018-08-17 17:33:19 +0300

    Add write script tags scope

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "216cadca2dcdbc17a64f464c03f76e1a"
   config.secret = "f78b4396435db2b7905c5fbe366340c9"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, write_script_tags"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
