0c5b5dde3a6ddb1c00b02dc56a00ac42bea56250
Alex Tsiras <tsirasalexis@yahoo.gr>
2018-08-17 17:44:25 +0300

    Register sample js file as script tag

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,4 +1,6 @@
 ShopifyApp.configure do |config|
+  include ActionView::Helpers::AssetUrlHelper
+
   config.application_name = "My Shopify App"
   config.api_key = "216cadca2dcdbc17a64f464c03f76e1a"
   config.secret = "f78b4396435db2b7905c5fbe366340c9"
@@ -6,4 +8,8 @@ ShopifyApp.configure do |config|
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
+
+  config.scripttags = [
+    {event: 'onload', src: -> (domain) { asset_url('my_file.js', host: ENV['APP_DOMAIN']) } }
+  ]
 end
